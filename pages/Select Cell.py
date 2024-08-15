97365a3900335a5459b81c2f1b41cee9a8517334
Reena <118504536+Reena9999@users.noreply.github.com>
2024-08-16 00:24:27 +0530

    Update Select Cell.py

--- pages/Select Cell.py
+++ pages/Select Cell.py
@@ -1,16 +1,20 @@
+# Page to view the different progressions of cell metrics over time
 import streamlit as st
 import requests
 import plotly.express as px
 import pandas as pd
 
+# estblishing the base url to send requests
 BASE_URL = "http://127.0.0.1:8080"
 
 def main():
     st.set_page_config(layout="wide")
+    # providing choice to select which cell to view details 
     selection = st.sidebar.selectbox(
         "Select Cell by its ID",
         (5308, 5329))
-    
+
+    # establishing request type to the API based on the selected cell ID
     endpoint = f"{BASE_URL}/read_data/{selection}"
 
     try:
@@ -20,10 +24,13 @@ def main():
         # Check if the response was successful
         if response.status_code == 200:
             data = response.json()
+            # converting the json response to a dataframe
             df = pd.DataFrame(data)
+            # changing datetime format for better visibility in graphs 
             df['absolute_time'] = pd.to_datetime(df['absolute_time']).dt.strftime('%Y-%m-%d %H:%M:%S')
-
+            # call function that prints the charts on the dashboard
             print_charts(df)
+            
         elif response.status_code == 404:
             st.error("Data not found")
         else:
@@ -31,7 +38,7 @@ def main():
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
-   
+# function to display charts
 def print_charts(df):
     fig1 = px.line(df,x='absolute_time', y='voltage_v',height=300,width=450,color_discrete_sequence=["#ff97ff"])
     fig1.update_layout(title='Time v. Voltage',xaxis_title="Time", yaxis_title="Voltage (V)", title_x=0.5)
